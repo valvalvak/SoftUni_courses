a0943c3f6011c1bda14dc5b9ef9f28d596656476
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-16 11:34:33 +0300

    update

--- 03.Advanced/01.Lists as Stacks and Queues/02. Exercise/02. Maximum and Minimum Element.py
+++ 03.Advanced/01.Lists as Stacks and Queues/02. Exercise/02. Maximum and Minimum Element.py
@@ -1,24 +1,56 @@
-from collections import deque
-
+# from collections import deque
+#
+# queries = int(input())
+#
+# queue = deque()
+#
+# for i in range(queries):
+#     current_case = list(map(int, input().split()))
+#     current_case = deque(current_case)
+#     command = current_case.popleft()
+#     if command == 1:
+#         queue.append(current_case.pop())
+#     elif command == 2:
+#         if queue:
+#             queue.pop()
+#     elif command == 3:
+#         print(max(queue))
+#     elif command == 4:
+#         print(min(queue))
+#
+# print(", ".join(map(str, reversed(queue))))
 queries = int(input())
 
-queue = deque()
+queue = []
+if not 1 <= queries <= 105:
+    exit(0)
 
 for i in range(queries):
     current_case = list(map(int, input().split()))
-    current_case = deque(current_case)
-    command = current_case.popleft()
+    command = current_case.pop(0)
+    if not 1 <= command <= 4:
+        continue
     if command == 1:
-        queue.append(current_case.pop())
+        value = current_case.pop()
+        if 1 <= value <= 109:
+            queue.insert(0, value)
     elif command == 2:
         if queue:
             queue.pop()
     elif command == 3:
-        print(max(queue))
+        if queue:
+            print(max(queue))
     elif command == 4:
-        print(min(queue))
+        if queue:
+            print(min(queue))
+    # elif command == 3:
+    #     if queue:
+    #         for x in range(queue.count(max(queue))):
+    #             print(max(queue))
+    # elif command == 4:
+    #     if queue:
+    #         for x in range(queue.count(min(queue))):
+    #             print(min(queue))
 
-result = ""
-while queue:
-    result += str(queue.pop()) + " "
-print(result.strip())
+if queue:
+    print(", ".join(map(str, queue)))
