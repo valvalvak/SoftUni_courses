a745852a3f486a27dd60deb425643932ec3149a9
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-18 00:16:34 +0300

    update

--- 03.Advanced/01.Lists as Stacks and Queues/02. Exercise/03. Fast Food.py
+++ 03.Advanced/01.Lists as Stacks and Queues/02. Exercise/03. Fast Food.py
@@ -8,22 +8,16 @@ que = deque()
 for order in orders:
     que.append(order)
 
-break_counter = 0
-
 while True:
     if quantity <= 0 or len(que) <= 0:
         break
-    current_order = que.popleft()
-    if current_order < quantity:
-        break_counter -= 1
-        if current_order == max(orders):
-            print(current_order)
+    current_order = que[0]
+    if current_order <= quantity:
+        que.popleft()
         quantity -= current_order
     else:
-        break_counter += 1
-        que.append(current_order)
-    if break_counter > len(que):
         break
+print(max(orders))
 if que:
     print(f"Orders left: {' '.join(map(str, que))}")
 else:
