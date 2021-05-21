8d0f9b466f407ac63877df5e06344417f9c53568
Valentin V. Vakrilov <77167373+valvalvak@users.noreply.github.com>
2021-05-21 13:24:38 +0300

    Update 05. Hot Potatos.py

--- 03.Advanced/01.Lists as Stacks and Queues/01. Lab/05. Hot Potatos.py
+++ 03.Advanced/01.Lists as Stacks and Queues/01. Lab/05. Hot Potatos.py
@@ -5,16 +5,6 @@ turn = int(input())
 
 q = deque(players)
 
-counter = 0
-
-# while len(q) > 1:
-#     counter += 1
-#     current_player = q.popleft()
-#     if counter == turn:
-#         print(f"Removed {current_player}")
-#     else:
-#         q.append(current_player)
-
 while len(q) > 1:
     for _ in range(turn - 1):
         q.append(q.popleft())
