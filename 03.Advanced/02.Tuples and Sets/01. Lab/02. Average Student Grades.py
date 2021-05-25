8365e33d42aac99c311bfe91ad167506b4f3d142
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-26 00:25:34 +0300

    update

--- 03.Advanced/02.Tuples and Sets/01. Lab/02. Average Student Grades.py
+++ 03.Advanced/02.Tuples and Sets/01. Lab/02. Average Student Grades.py
@@ -1,9 +1,40 @@
-students = int(input())
+def input_to_tuple(count):
+    lines = list()
+    for _ in range(count):
+        lines.append(input())
+    return lines
 
-all_students = {}
 
-for values in range(students):
-    student, grade = input().split(" ")
-    if student not in all_students:
-        # all_students[student]
-        pass
+n = int(input())
+students_grades_lines = input_to_tuple(n)
+
+students_grades = {}
+
+
+# n = int("7")
+#
+# students_grades_lines = (
+#     'Peter 5.20',
+#     'Mark 5.50',
+#     'Peter 3.20',
+#     'Mark 2.50',
+#     'Alex 2.00',
+#     'Mark 3.46',
+#     'Alex 3.00'
+# )
+
+def avg_grade(grades):
+    return sum(grades) / len(grades)
+
+
+for line in students_grades_lines:
+    students, grade = line.split()
+    if students not in students_grades:
+        students_grades[students] = float(grade),
+    else:
+        students_grades[students] += float(grade),
+
+for (student, grades) in students_grades.items():
+    student_grades_list = ' '.join(map(lambda grade: f"{grade:.2f}", grades))
+    student_average_grade = avg_grade(grades)
+    print(f"{student} -> {student_grades_list} (avg: {student_average_grade:.2f})")
