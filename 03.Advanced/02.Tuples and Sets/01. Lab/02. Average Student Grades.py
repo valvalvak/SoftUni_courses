72ae4d8237e42389399490c339241e518a820b31
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-25 21:34:34 +0300

    update

--- 03.Advanced/02.Tuples and Sets/01. Lab/02. Average Student Grades.py
+++ 03.Advanced/02.Tuples and Sets/01. Lab/02. Average Student Grades.py
@@ -1,6 +1,9 @@
-n = int(input())
-names = set()
-for _ in range(n):
-    names.add(input())
-for name in names:
-    print(name)
+students = int(input())
+
+all_students = {}
+
+for values in range(students):
+    student, grade = input().split(" ")
+    if student not in all_students:
+        # all_students[student]
+        pass
---
+++ 03.Advanced/02.Tuples and Sets/01. Lab/03. Record Unique Names.py
@@ -0,0 +1,6 @@
+n = int(input())
+names = set()
+for _ in range(n):
+    names.add(input())
+for name in names:
+    print(name)
