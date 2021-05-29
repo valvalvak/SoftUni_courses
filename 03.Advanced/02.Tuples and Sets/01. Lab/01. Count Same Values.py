1053fa94e7302d0fd7e6dab95601aa26851760db
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-29 12:47:42 +0300

    form home

--- 03.Advanced/02.Tuples and Sets/01. Lab/01. Count Same Values.py
+++ 03.Advanced/02.Tuples and Sets/01. Lab/01. Count Same Values.py
@@ -1,8 +1,12 @@
 values = tuple(input().split(" "))
+
 counter_dictionary = {}
+
 for el in values:
+
     if el not in counter_dictionary:
         counter_dictionary[el] = 0
     counter_dictionary[el] += 1
+
 for num, count in counter_dictionary.items():
     print(f"{float(num)} - {count} times")
