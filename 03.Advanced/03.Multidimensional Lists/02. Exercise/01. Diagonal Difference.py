1fb5e0bd1ce183c1fa6bab8656557eb1c4f2c640
Valentin Vakrilov <vvakrilov@gmail.com>
2021-05-31 18:54:19 +0300

    basic

---
+++ 03.Advanced/03.Multidimensional Lists/02. Exercise/01. Diagonal Difference.py
@@ -0,0 +1,30 @@
+def read_matrix(rows):
+    matrix_build = []
+
+    for i in range(rows):
+        matrix_build.append([int(x) for x in input().split(" ")])
+
+    return matrix_build
+
+
+def prime_diagonal_sum(matrix):
+    result = 0
+    size = len(matrix)
+    for i in range(size):
+        result += matrix[i][i]
+    return result
+
+
+def mirror_diagonal_sum(matrix):
+    result = 0
+    size = len(matrix)
+    for i in range(size):
+        result += matrix[i][size - i - 1]
+    return result
+
+
+matrix = read_matrix(int(input()))
+
+result = abs(prime_diagonal_sum(matrix) - mirror_diagonal_sum(matrix))
+
+print(result)
