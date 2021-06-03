6b97c83706f487dd8d2877c8edab5c077df84240
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-03 16:33:32 +0300

    from home

---
+++ 03.Advanced/03.Multidimensional Lists/02. Exercise/05. Snake Moves.py
@@ -0,0 +1,9 @@
+def snake_moves():
+    pass
+
+
+row, col = [int(x) for x in input().split(' ')]
+snake_as_list_from_string = list(x for x in input())
+
+print(row, col)
+print(snake_as_list_from_string)
