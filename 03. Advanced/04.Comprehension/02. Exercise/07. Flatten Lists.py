314e001cac41f7c86032fb8ec6771c4dfe670b06
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-08 00:54:17 +0300

    from home

--- 03. Advanced/04.Comprehension/02. Exercise/07. Flatten Lists.py
+++ 03. Advanced/04.Comprehension/02. Exercise/07. Flatten Lists.py
@@ -1,31 +1,27 @@
-# ==========================================================================================
-# not working (75/100), and do not know why?! doesn't work with "reverse()" and/or reversed()
-# ==========================================================================================
-# def read_input():
-#     raw_input = [x for x in input().split("|") if len(x) > 0]
-#     result = [[int(x) for x in row.split()] for row in raw_input if row]
-#     return result
-#
-#
-# def to_do_task():
-#     result = read_input()
-#     result.reverse()
-#     return result
-#
-#
-# def print_issue():
-#     issue = to_do_task()
-#
-#     print(f"{x}", sep=" ")
-# print_issue()
+def read_input():
+    raw_input = [x for x in input().split("|")]
+    result = [row.split() for row in raw_input]
+    return result
 
-# working just fine: 100/100
-# ok, but why?
 
-input_str = input().split("|")[::-1]
+def to_do_task():
+    result = read_input()
+    result.reverse()
+    return result
 
-matrix = [x.split() for x in input_str]
 
-flatten_list = [j for i in matrix for j in i]
+def print_issue():
+    issue = to_do_task()
+    print(' '.join(' '.join([x for x in row]) for row in issue if len(row) > 0))
 
-print(" ".join(flatten_list))
+
+print_issue()
+
+#
+# input_str = input().split("|")[::-1]
+#
+# matrix = [x.split() for x in input_str]
+#
+# flatten_list = [j for i in matrix for j in i]
+#
+# print(" ".join(flatten_list))
