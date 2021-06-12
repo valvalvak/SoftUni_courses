0988498ef7ea95f605d93adc6b98330a77f13b33
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-12 10:51:38 +0300

    form home

---
+++ 03. Advanced/06. File Handling/02. Exercise/Homework/01. Even_lines/01.1 Even Lines_v1.py
@@ -0,0 +1,18 @@
+"""
+Write a program that reads a text file and prints on the console its even lines.
+Line numbers start from 0.
+Before you print the result replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
+==========
+Variant: 1
+==========
+"""
+
+STRING_SET = {"-", ",", ".", "!", "?"}
+
+with open("test.txt", "r") as file:
+    for idx, row in enumerate(file):
+        if idx % 2 == 0:
+            for el in STRING_SET:
+                row = row.replace(el, "@")
+            words = reversed(row.split())
+            print(" ".join(words))
---
+++ 03. Advanced/06. File Handling/02. Exercise/Homework/01. Even_lines/01.2 Even Lines_v2.py
@@ -0,0 +1,20 @@
+"""
+Write a program that reads a text file and prints on the console its even lines.
+Line numbers start from 0.
+Before you print the result replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
+==========
+Variant: 2
+==========
+"""
+import re
+
+
+def replace_symbol_regex(row):
+    return re.sub(r"[-,.!?]", "@", row)
+
+
+with open("test.txt", "r") as file:
+    for idx, row in enumerate(file):
+        if idx % 2 == 0:
+            replaced_row = replace_symbol_regex(row).split()
+            print(" ".join(replaced_row[::-1]))
