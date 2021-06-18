a6598da50e491d84fd0ba7be059ebebe686664ef
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-06-18 12:16:26 +0300

    update (w)

---
+++ 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling.py
@@ -0,0 +1,23 @@
+# from collections import deque
+
+# def get_list_of_jobs_cycles():
+#     jobs_list = [int(x) for x in input().split(", ")]
+#     return jobs_list
+
+# def get_count_of_all_tasks_to_target(tasks, target):
+#     clock_cycle = 0
+#     priority_task = min(tasks)
+#     current_task = tasks[0]
+    
+#     while tasks:
+#         if current_task == priority_task == target:
+#             return clock_cycle
+#         if current_task == priority_task:
+#             clock_cycle +=1
+#             tasks.popleft()
+ 
+        
+
+# task_que = deque(get_list_of_jobs_cycles())
+# target_task = int(input())
+# print(get_count_of_all_tasks_to_target(task_que, task_que[target_task]))
