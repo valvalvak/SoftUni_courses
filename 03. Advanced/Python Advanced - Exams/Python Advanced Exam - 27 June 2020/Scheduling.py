4146c37c2bea171265d6f1dbe75e8274754e2525
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-06-18 12:31:56 +0300

    update (w)

--- 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling.py
+++ 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling.py
@@ -1,23 +1,26 @@
-# from collections import deque
+from collections import deque
 
-# def get_list_of_jobs_cycles():
-#     jobs_list = [int(x) for x in input().split(", ")]
-#     return jobs_list
+def get_list_of_jobs_cycles():
+    jobs_list = [int(x) for x in input().split(", ")]
+    return jobs_list
 
-# def get_count_of_all_tasks_to_target(tasks, target):
-#     clock_cycle = 0
-#     priority_task = min(tasks)
-#     current_task = tasks[0]
+def get_count_of_all_tasks_to_target(tasks, target):
+    clock_cycle = 0
+    priority_task = min(tasks)
+    current_task = tasks[0]
     
-#     while tasks:
-#         if current_task == priority_task == target:
-#             return clock_cycle
-#         if current_task == priority_task:
-#             clock_cycle +=1
-#             tasks.popleft()
+    while tasks:
+        if current_task == priority_task == target:
+            return clock_cycle
+        else:
+            clock_cycle +=1
+            if not current_task == priority_task:
+                tasks.rotate(-1)
+            else:
+                tasks.popleft()
  
         
 
-# task_que = deque(get_list_of_jobs_cycles())
-# target_task = int(input())
-# print(get_count_of_all_tasks_to_target(task_que, task_que[target_task]))
+task_que = deque(get_list_of_jobs_cycles())
+target_task = int(input())
+print(get_count_of_all_tasks_to_target(task_que, task_que[target_task]))
