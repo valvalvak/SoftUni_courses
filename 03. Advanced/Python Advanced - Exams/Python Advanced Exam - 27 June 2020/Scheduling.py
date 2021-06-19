c904cbf419dd2e77cbe276edddca4b6d2b2bb733
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-19 16:42:11 +0300

    update (h)

--- 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling.py
+++ 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling.py
@@ -2,18 +2,19 @@ def get_list_of_jobs_cycles(task_jobs):
     return [int(x) for x in task_jobs.split(", ")]
 
 
-def get_idex_of_aimed_task(value):
+def get_idex_of_aimed_task_sort(value):
     while True:
         try:
             aimed_task_job = int(value)
             return aimed_task_job
         except ValueError:
-            print(f"Must type intiger:{get_idex_of_aimed_task(input())}")
+            print(f"Must type intiger:{get_idex_of_aimed_task_sort(input())}")
 
 
 def count_cycles(tasks, aimed_task):
     counter = 0
+    pass
 
 
 tasks_q = get_list_of_jobs_cycles(input())
-aimed_task = get_idex_of_aimed_task(input())
+aimed_task = get_idex_of_aimed_task_sort(input())
--- 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling_copy.py
+++ 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 27 June 2020/Scheduling_copy.py
@@ -1,9 +1,12 @@
+from heapq import heappop, heappush
+
+
 def get_list_of_jobs_cycles(_input_):
     task_list = [int(x) for x in _input_.split(", ")]
     return task_list
 
 
-def get_count_of_all_tasks_to_target(tasks, target):
+def get_count_of_all_tasks_to_target_sorted(tasks, target):
     sorted_tasks = sorted([(val, idx) for (idx, val) in enumerate(tasks)])
 
     cycles = 0
@@ -15,6 +18,23 @@ def get_count_of_all_tasks_to_target(tasks, target):
     return cycles
 
 
+def get_count_of_all_tasks_to_target_heap(tasks, target):
+    heap = []
+    for (idx, task) in enumerate(tasks):
+        heappush(heap, (task, idx))
+
+    cycles = 0
+
+    while True:
+        (task, idx) = heappop(heap)
+        cycles += task
+        if idx == target:
+            break
+
+    return cycles
+
+
 task_que = get_list_of_jobs_cycles(input())
 target_task = int(input())
-print(get_count_of_all_tasks_to_target(task_que, target_task))
+print(get_count_of_all_tasks_to_target_sorted(task_que, target_task))
+print(get_count_of_all_tasks_to_target_heap(task_que, target_task))
