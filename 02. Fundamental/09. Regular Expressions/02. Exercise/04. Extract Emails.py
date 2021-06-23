7c61b5909145073a07ad700eb3c7837ef5821114
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-23 23:52:23 +0300

    update (h)

--- 02. Fundamental/09. Regular Expressions/02. Exercise/04. Extract Emails.py
+++ 02. Fundamental/09. Regular Expressions/02. Exercise/04. Extract Emails.py
@@ -9,3 +9,4 @@ regex = r"(^|(?<=\s))[a-zA-Z0-9]+[\._-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+"
 result = [obj.group() for obj in re.finditer(regex, message)]
 
 print(*result, sep="\n")
+print("".join(result))
