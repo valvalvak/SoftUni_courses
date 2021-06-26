d34ae696c2d6e429f6becd977fd67c4d1918b7d6
Valentin Vakrilov <vvakrilov@gmail.com>
2021-06-26 21:06:38 +0300

    Update 03.Cupcake shop.py

---
+++ 03. Advanced/Python Advanced - Exams/Python Advanced Exam - 14 February 2021/03.Cupcake shop.py
@@ -0,0 +1,13 @@
+def stock_availability(param, param1, param2, param3):
+    cupcake_flavours = []
+    
+    pass
+
+
+print(stock_availability(["choco", "vanilla", "banana"], "delivery", "caramel", "berry"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "delivery", "cookie","banana"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell", 3))
+print(stock_availability(["chocolate", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(["cookie", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(["chocolate", "vanilla", "banana"], "sell", "cookie"))
