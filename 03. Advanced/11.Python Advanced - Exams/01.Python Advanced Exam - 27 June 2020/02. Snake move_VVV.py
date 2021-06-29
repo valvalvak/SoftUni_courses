967b80785e1a2a548d30fdfe12c161efd5e19680
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-06-29 16:44:50 +0300

    UPDATE (W)

--- 03. Advanced/11.Python Advanced - Exams/01.Python Advanced Exam - 27 June 2020/02. Snake move_VVV.py
+++ 03. Advanced/11.Python Advanced - Exams/01.Python Advanced Exam - 27 June 2020/02. Snake move_VVV.py
@@ -1,6 +1,8 @@
+from collections import deque
+
 SNAKE = "S"
 FOOD = "*"
-LAIR = "B"
+BURROW = "B"
 EMPTY = "-"
 MOVE_DIRECTIONS = {"up": (-1, 0), "down": (1, 0), "left": (0, -1), "right": (0, 1), }
 
@@ -25,6 +27,15 @@ def get_snake_position(terrarium):
                 return snake_position
 
 
+def get_burrow_position(terrarium):
+    burrow_indices = deque()
+    for row_idx, row in enumerate(terrarium):
+        for col_idx, col in enumerate(row):
+            if BURROW in col:
+                burrow_indices.append((row_idx, col_idx))
+                return burrow_indices
+
+
 def get_new_indices(snake_position, move_indices):
     snake_row_pos, snake_col_pos = snake_position
     destination_row, destination_col = move_indices
@@ -32,10 +43,15 @@ def get_new_indices(snake_position, move_indices):
     return (new_row_idx, new_col_idx)
 
 
-def in_range(terrarium, new_cell_indices):
-    new_row_idx, new_col_idx = new_cell_indices
-    size = len(terrarium)
-    return 0 <= new_row_idx < size and 0 <= new_col_idx < size
+def in_range(terrarium, new_row_idx, new_col_idx):
+    return 0 <= new_row_idx < len(terrarium) and 0 <= new_col_idx < len(terrarium)
+
+
+def get_burrow_pass(burrow_indices_que, new_row_idx, new_col_idx):
+    burrow_row_idx, burrow_col_idx = burrow_indices_que.popleft()
+    if burrow_row_idx == new_row_idx and burrow_col_idx == new_col_idx:
+        burrow_row_idx, burrow_col_idx = burrow_indices_que.popleft()
+    return burrow_row_idx, burrow_col_idx
 
 
 def get_move_commands(terrarium):
@@ -43,13 +59,29 @@ def get_move_commands(terrarium):
     Each turn, you will be given command for the snake’s movement.
     When the snake moves it leaves a trail marked with '.'
     """
+    food_count = 0
     snake_position = get_snake_position(terrarium)
+    burrow_indices_que = get_burrow_position(terrarium)
+
     while True:
+
         move_indices = get_move_indices(input())
-        new_cell_indices = get_new_indices(snake_position, move_indices)
-        if not in_range(terrarium, new_cell_indices):
-            pass
-        
+        new_row_idx, new_col_idx = get_new_indices(snake_position, move_indices)
+
+        if terrarium[new_row_idx][new_col_idx] == BURROW:
+            terrarium[new_row_idx][new_col_idx] = EMPTY
+            new_row_idx, new_col_idx = get_burrow_pass(burrow_indices_que, new_row_idx, new_col_idx)
+
+        if not in_range(terrarium, new_row_idx, new_col_idx):
+            continue
+
+        empty_row_idx, empty_col_idx = snake_position
+
+        if terrarium[new_row_idx][new_col_idx] == FOOD:
+            food_count += 1
+
+        terrarium[empty_col_idx][empty_col_idx] = EMPTY
+        terrarium[new_row_idx][new_col_idx] = SNAKE
 
 
 square_size = int(input())
