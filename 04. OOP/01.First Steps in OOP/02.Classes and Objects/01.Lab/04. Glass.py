775e631382e985dd197e9e43cff51a77950b4eb7
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-03 20:14:09 +0300

    update homework (h)

---
+++ 04. OOP/01.First Steps in OOP/02.Classes and Objects/01.Lab/04. Glass.py
@@ -0,0 +1,21 @@
+class Glass:
+    def __init__(self):
+        pass
+
+    def fill(self, param):
+        pass
+
+    def empty(self):
+        pass
+
+    def info(self):
+        pass
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
+
