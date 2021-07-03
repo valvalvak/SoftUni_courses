fe6dc3af86133b34014e03350c11711983bf55f3
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-03 20:38:52 +0300

    update (h)

--- 04. OOP/01.First Steps in OOP/02. Exercise/project/trainer.py
+++ 04. OOP/01.First Steps in OOP/02. Exercise/project/trainer.py
@@ -1,5 +1,6 @@
 from typing import List
-from pokemon import Pokemon
+
+from project.pokemon import Pokemon
 
 
 class Trainer:
@@ -12,7 +13,7 @@ class Trainer:
             return "This pokemon is already caught"
         else:
             self.pokemons.append(pokemon)
-            return f"Caught {Pokemon.pokemon_details(pokemon)}"
+            return f"Caught {pokemon.pokemon_details(pokemon)}"
 
     def release_pokemon(self, pokemon_name):
         for pokemon in self.pokemons:
@@ -30,25 +31,25 @@ class Trainer:
         #     f"- {nl.join([pokemon.pokemon_details() for pokemon in self.pokemons])}"
         print_solution = ''
         print_solution += f'Pokemon Trainer {self.name}{nl}'
-        print_solution += f'Pokemon count {len(self.pokemons)}'
+        print_solution += f'Pokemon count {len(self.pokemons)}{nl}'
         for pokemon in self.pokemons:
-            print_solution += f'- {nl}{pokemon.pokemon_details()}'
+            print_solution += f'- {pokemon.pokemon_details()}{nl}'
         return print_solution
 
 
-# pokemon = Pokemon("Pikachu", 90)
-# print(pokemon.pokemon_details())
-# trainer = Trainer("Ash")
-# print(trainer.add_pokemon(pokemon))
-# second_pokemon = Pokemon("Charizard", 110)
-# second3_pokemon = Pokemon("Charizard3", 330)
-# second4_pokemon = Pokemon("Charizard4", 440)
-# second5_pokemon = Pokemon("Charizard5", 555)
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.add_pokemon(second3_pokemon))
-# print(trainer.add_pokemon(second4_pokemon))
-# print(trainer.add_pokemon(second5_pokemon))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.trainer_data())
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+second3_pokemon = Pokemon("Charizard3", 330)
+second4_pokemon = Pokemon("Charizard4", 440)
+second5_pokemon = Pokemon("Charizard5", 555)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second3_pokemon))
+print(trainer.add_pokemon(second4_pokemon))
+print(trainer.add_pokemon(second5_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
