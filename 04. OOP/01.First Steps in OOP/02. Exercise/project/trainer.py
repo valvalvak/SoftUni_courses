a3aee153fe13a518901a39c7928b971da613656a
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-04 22:17:02 +0300

    update (h)

--- 04. OOP/01.First Steps in OOP/02. Exercise/project/trainer.py
+++ 04. OOP/01.First Steps in OOP/02. Exercise/project/trainer.py
@@ -20,8 +20,7 @@ class Trainer:
             if pokemon.name == pokemon_name:
                 self.pokemons.remove(pokemon)
                 return f"You have released {pokemon_name}"
-            else:
-                return "Pokemon is not caught"
+        return "Pokemon is not caught"
 
     def trainer_data(self):
         nl = "\n"
@@ -33,23 +32,22 @@ class Trainer:
         print_solution += f'Pokemon Trainer {self.name}{nl}'
         print_solution += f'Pokemon count {len(self.pokemons)}{nl}'
         for pokemon in self.pokemons:
-            print_solution += f'- {pokemon.pokemon_details()}'
+            print_solution += f'- {pokemon.pokemon_details()}{nl}'
         return print_solution
 
-
-pokemon = Pokemon("Pikachu", 90)
-print(pokemon.pokemon_details())
-trainer = Trainer("Ash")
-print(trainer.add_pokemon(pokemon))
-second_pokemon = Pokemon("Charizard", 110)
-second3_pokemon = Pokemon("Charizard3", 330)
-second4_pokemon = Pokemon("Charizard4", 440)
-second5_pokemon = Pokemon("Charizard5", 555)
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.add_pokemon(second3_pokemon))
-print(trainer.add_pokemon(second4_pokemon))
-print(trainer.add_pokemon(second5_pokemon))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.trainer_data())
+# pokemon = Pokemon("Pikachu", 90)
+# print(pokemon.pokemon_details())
+# trainer = Trainer("Ash")
+# print(trainer.add_pokemon(pokemon))
+# second_pokemon = Pokemon("Charizard", 110)
+# second3_pokemon = Pokemon("Charizard3", 330)
+# second4_pokemon = Pokemon("Charizard4", 440)
+# second5_pokemon = Pokemon("Charizard5", 555)
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.add_pokemon(second3_pokemon))
+# print(trainer.add_pokemon(second4_pokemon))
+# print(trainer.add_pokemon(second5_pokemon))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.trainer_data())
