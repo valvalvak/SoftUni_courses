550febcf7298069fbf03c5be7fc2c0cfb9c5246b
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-05 02:03:51 +0300

    update (h) (not finished, working on)

---
+++ 04. OOP/02.Classes and Objects/02.Exercise/05. To-do List/project/section.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Section:
+    def __init__(self, name: str):
+        self.name = name
+        self.task: List = []
+
+    def add_task(self, new_task):
+        self.task.append(new_task)
+        return f"Task {Task.details(new_task)} is added to the section"
+
+    def clean_section(self):
+        pass
+
+    def view_section(self):
+        pass
+
+
+if __name__ == '__main__':
+    from task import Task as Task
