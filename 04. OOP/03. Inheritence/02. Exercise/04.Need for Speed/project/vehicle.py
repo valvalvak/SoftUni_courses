ce94d3301caf41ad2316ff35d24a4c80fc9823be
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-11 01:03:46 +0300

    update (h) (done100/100)

---
+++ 04. OOP/03. Inheritence/02. Exercise/04.Need for Speed/project/vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+
+    def drive(self, kilometers):
+        if not self.fuel - self.fuel_consumption * kilometers < 0:
+            self.fuel -= self.fuel - self.fuel_consumption * kilometers
+            return True
+        return False
