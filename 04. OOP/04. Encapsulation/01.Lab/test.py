cd3e45e2dc91ad08f948329eb76c6abd2802372f
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-11 22:23:51 +0300

    update (h) (done100/100)

---
+++ 04. OOP/04. Encapsulation/01.Lab/test.py
@@ -0,0 +1,14 @@
+daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+number = 8
+try:
+    print(daysOfWeek[number])
+except IndexError:
+    print("Index Error VVV")
