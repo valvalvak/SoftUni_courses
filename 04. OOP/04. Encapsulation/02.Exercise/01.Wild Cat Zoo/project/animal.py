f85a50d2cae89baba8f4aa37d532e9dd972fed52
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-12 02:13:27 +0300

    homework update

--- 04. OOP/04. Encapsulation/02.Exercise/01.Wild Cat Zoo/project/animal.py
+++ 04. OOP/04. Encapsulation/02.Exercise/01.Wild Cat Zoo/project/animal.py
@@ -1,5 +1,9 @@
 class Animal:
-    def __init__(self, name: str, gender: str, age: int):
+    def __init__(self, name: str, gender: str, age: int, money_for_care: int):
+        self.money_for_care = money_for_care
         self.name = name
         self.gender = gender
         self.age = age
+
+    def __repr__(self):
+        return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
---
+++ 04. OOP/04. Encapsulation/02.Exercise/01.Wild Cat Zoo/project/zoo.py
@@ -0,0 +1,63 @@
+from typing import List
+
+
+class Zoo:
+    def __init__(self, name: str, budget: int, animal_capacity: int, worker_capacity: int):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__worker_capacity = worker_capacity
+        self.animals: List = []
+        self.workers: List = []
+
+    def add_animal(self, animal, price):
+        pass
+
+    def hire_worker(self, worker):
+        pass
+
+    def fire_worker(self, worker_name):
+        pass
+
+    def pay_workers(self):
+        pass
+
+    def tend_animals(self):
+        pass
+
+    def profit(self, amount):
+        pass
+
+    def animals_status(self):
+        """
+        :return:
+        You have {total_animals_count} animals
+        ----- {amount_of_lions} Lions:
+        {lion1}
+        ...
+        ----- {amount_of_tigers} Tigers:
+        {tiger1}
+        ...
+        ----- {amount_of_cheetahs} Cheetahs:
+        {cheetah1}
+        ...
+        Hint: use the __repr__ methods of the animals to print them on the console
+        """
+        pass
+
+    def worker_status(self):
+        """
+        :return:
+        You have {total_workers_count} workers
+        ----- {amount_of_keepers} Keepers:
+        {keeper1}
+        ...
+        ----- {amount_of_caretakers} Caretakers:
+        {caretaker1}
+        ...
+        ----- {amount_of_vetes} Vets:
+        {vet1}
+        ...
+        Hint: use the __repr__ methods of the workers to print them on the console
+        """
+        pass
