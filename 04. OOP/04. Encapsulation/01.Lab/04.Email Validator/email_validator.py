41cd0305c9de7ba95264932ed6aef68a9e1d864a
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-12 00:55:28 +0300

    homework update

---
+++ 04. OOP/04. Encapsulation/01.Lab/04.Email Validator/email_validator.py
@@ -0,0 +1,33 @@
+import re
+
+
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, username):
+        return self.min_length <= len(username)
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        username, mail, domain = re.split('[@.]', email)
+        if not all([EmailValidator.__is_name_valid(self, username),
+                    EmailValidator.__is_mail_valid(self, mail),
+                    EmailValidator.__is_domain_valid(self, domain)]):
+            return False
+        return True
+
+# mails = ["gmail", "softuni"]
+# domains = ["com", "bg"]
+# email_validator = EmailValidator(6, mails, domains)
+# print(email_validator.validate("pe77er@gmail.com"))
+# print(email_validator.validate("georgios@gmail.net"))
+# print(email_validator.validate("stamatito@abv.net"))
+# print(email_validator.validate("abv@softuni.bg"))
