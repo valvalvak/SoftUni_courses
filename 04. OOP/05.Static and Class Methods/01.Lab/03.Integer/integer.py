38980ce6968a6dd979ad7c73bbcb822faf667f90
valvalvak <vvakrilov@gmail.com>
2021-07-17 19:22:21 +0300

    update (h)

--- 04. OOP/05.Static and Class Methods/01.Lab/03.Integer/integer.py
+++ 04. OOP/05.Static and Class Methods/01.Lab/03.Integer/integer.py
@@ -1,54 +1,47 @@
-from math import floor
-
-
 class Integer:
     def __init__(self, value: int):
         self.value = value
 
     @classmethod
     def from_float(cls, float_value):
-        if isinstance(float_value, float):
-            return cls(floor(float_value))
-        return f"value is not a float"
+        if not isinstance(float_value, float):
+            return "value is not a float"
+        return cls(int(float_value))
 
     @classmethod
     def from_roman(cls, value):
         """ Convert a Roman numeral to an integer. """
+        if not isinstance(value, str):
+            return "wrong type"
+            # raise TypeError("expected string, got type(value))
+        value = value.upper()
+        nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        num = 0
+        for i in range(len(value)):
+            current_value = nums[value[i]]
+            # If the next place holds a larger number, this value is negative
+            if i + 1 < len(value) and nums[value[i + 1]] > current_value:
+                num -= current_value
+            else:
+                num += current_value
+        return cls(num)
 
-        if not isinstance(value, type("")):
-            raise TypeError, "expected string, got %s" % type(input)
-        input = input.upper(  )
-        nums = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
-        sum = 0
-        for i in range(len(input)):
-            try:
-                value = nums[input[i]]
-                # If the next place holds a larger number, this value is negative
-                if i+1 < len(input) and nums[input[i+1]] > value:
-                    sum -= value
-                else: sum += value
-            except KeyError:
-                raise ValueError, 'input is not a valid Roman numeral: %s' % input
-        # easiest test for validity...
-        if int_to_roman(sum) == input:
-            return sum
-        else:
-            raise ValueError, 'input is not a valid Roman numeral: %s' % input
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return "wrong type"
+        try:
+            return cls(int(value))
+        except:
+            return "wrong type"
 
-@classmethod
-def from_sting(cls, value):
-    pass
-
-@classmethod
-def from_string(cls, param):
-    pass
+# first_num = Integer(10)
+# second_num = Integer.from_roman("IV")
+#
+# print(Integer.from_float("2.6"))
+# print(Integer.from_string(2.6))
 
 
-first_num = Integer(10)
-second_num = Integer.from_roman("IV")
-
-print(Integer.from_float("2.6", ))
-print(Integer.from_string(2.6))
 # def int_to_roman(input):
 #     """ Convert an integer to a Roman numeral. """
 #
@@ -112,3 +105,53 @@ print(Integer.from_string(2.6))
 #     if index < len(input): # There are characters unaccounted for
 #         raise ValueError, 'input is not a valid roman numeral: %s'%input
 #     return result
+# roman = {
+#             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+#             'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
+#         }
+#         i = 0
+#         num = 0
+#         while i < len(value):
+#             if i + 1 < len(value) and value[i:i + 2] in roman:
+#                 num += roman[value[i:i + 2]]
+#                 i += 2
+#             else:
+#                 # print(i)
+#                 num += roman[value[i]]
+#                 i += 1
+#         return cls(num)
+
+
+####
+# # unittests
+# import unittest
+#
+#
+# class IntegerTests(unittest.TestCase):
+#     def test_basic_init(self):
+#         integer = Integer(1)
+#         self.assertEqual(integer.value, 1)
+#
+#     def test_from_float_success(self):
+#         integer = Integer.from_float(2.5)
+#         self.assertEqual(integer.value, 2)
+#
+#     def test_from_float_wrong_type(self):
+#         result = Integer.from_float("2.5")
+#         self.assertEqual(result, "value is not a float")
+#
+#     def test_from_roman(self):
+#         integer = Integer.from_roman("XIX")
+#         self.assertEqual(integer.value, 19)
+#
+#     def test_from_string_success(self):
+#         integer = Integer.from_string("10")
+#         self.assertEqual(integer.value, 10)
+#
+#     def test_from_string_wrong_type(self):
+#         result = Integer.from_string(1.5)
+#         self.assertEqual(result, "wrong type")
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
