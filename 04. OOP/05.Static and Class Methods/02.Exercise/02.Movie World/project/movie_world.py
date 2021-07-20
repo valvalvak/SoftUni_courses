7c812f1b25b7315d6c93cb4569e28bb42c20a055
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-20 21:34:25 +0300

    homework update

---
+++ 04. OOP/05.Static and Class Methods/02.Exercise/02.Movie World/project/movie_world.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from project.customer import Customer
+from project.dvd import DVD
+
+
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers: List = [Customer]
+        self.dvds: List = [DVD]
