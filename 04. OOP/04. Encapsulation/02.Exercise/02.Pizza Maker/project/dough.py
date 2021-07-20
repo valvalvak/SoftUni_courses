944e661ee131b86db1aeb8ceed3981025c62eb20
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-20 20:24:41 +0300

    homework update

--- 04. OOP/04. Encapsulation/02.Exercise/02.Pizza Maker/project/dough.py
+++ 04. OOP/04. Encapsulation/02.Exercise/02.Pizza Maker/project/dough.py
@@ -6,30 +6,30 @@ class Dough:
 
     @property
     def flour_type(self):
-        return self.flour_type
+        return self.__flour_type
 
     @flour_type.setter
     def flour_type(self, value):
         if len(value) < 1:
             raise ValueError("The flour type cannot be an empty string")
-        self.flour_type = value
+        self.__flour_type = value
 
     @property
     def baking_technique(self):
-        return self.baking_technique
+        return self.__baking_technique
 
     @baking_technique.setter
     def baking_technique(self, value):
         if len(value) < 1:
             raise ValueError("The baking technique cannot be an empty string")
-        self.baking_technique = value
+        self.__baking_technique = value
 
     @property
     def weight(self):
-        return self.weight
+        return self.__weight
 
     @weight.setter
     def weight(self, value):
         if value < 1:
             raise ValueError("The weight cannot be less or equal to zero")
-        self.weight = value
+        self.__weight = value
--- 04. OOP/04. Encapsulation/02.Exercise/02.Pizza Maker/project/pizza.py
+++ 04. OOP/04. Encapsulation/02.Exercise/02.Pizza Maker/project/pizza.py
@@ -1,46 +1,56 @@
-from project.dough import Dough
-from project.topping import Topping
+# from math import floor
+#
+# from project.dough import Dough
+# from project.topping import Topping
+#
+#
+# class Pizza:
+#     def __init__(self, name: str, dough: Dough, toppings_capacity: int, toppings: dict):
+#         self.name = name
+#         self.dough = dough
+#         self.toppings_capacity = toppings_capacity
+#         self.toppings = toppings
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, value):
+#         if len(value) < 1:
+#             raise ValueError("The name cannot be an empty string")
+#         self.__name = value
+#
+#     @property
+#     def dough(self):
+#         return self.__dough
+#
+#     @dough.setter
+#     def dough(self, value):
+#         if value is None:
+#             raise ValueError("You should add dough to the pizza")
+#         self.__dough = value
+#
+#     @property
+#     def topping_capacity(self):
+#         return self.__toppings_capacity
+#
+#     @topping_capacity.setter
+#     def topping_capacity(self, value):
+#         if value < 1:
+#             raise ValueError("The topping's capacity cannot be less or equal to zero")
+#         self.__toppings_capacity = value
+#
+#     def add_topping(self, topping: Topping):
+#         pass
+#
+#     def calculate_total_weight(self):
+#         pass
+from math import floor
 
+a = round(5.4999999999999995)
+b = round(5.4999999999999996)
+c = floor(5.4999999999999995)
+d = floor(5.4999999999999996)
 
-class Pizza:
-    def __init__(self, name: str, dough: Dough, toppings_capacity: int, toppings: dict):
-        self.name = name
-        self.dough = dough
-        self.toppings_capacity = toppings_capacity
-        self.toppings = toppings
-
-    @property
-    def name(self):
-        return self.name
-
-    @name.setter
-    def name(self, value):
-        if len(value) < 1:
-            raise ValueError("The name cannot be an empty string")
-        self.name = value
-
-    @property
-    def dough(self):
-        return self.dough
-
-    @dough.setter
-    def dough(self, value):
-        if value is None:
-            raise ValueError("You should add dough to the pizza")
-        self.dough = value
-
-    @property
-    def topping_capacity(self):
-        return self.toppings_capacity
-
-    @topping_capacity.setter
-    def topping_capacity(self, value):
-        if value < 1:
-            raise ValueError("The topping's capacity cannot be less or equal to zero")
-        self.topping_capacity = value
-
-    def add_topping(self, topping: Topping):
-        pass
-
-    def calculate_total_weight(self):
-        pass
+print(a, b, c, d)
