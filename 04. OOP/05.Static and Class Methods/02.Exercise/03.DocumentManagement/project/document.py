27143b44f74cfbfe996b9b2071319df04d44c910
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-23 12:26:48 +0300

    homework update

--- 04. OOP/05.Static and Class Methods/02.Exercise/03.DocumentManagement/project/document.py
+++ 04. OOP/05.Static and Class Methods/02.Exercise/03.DocumentManagement/project/document.py
@@ -8,10 +8,10 @@ class Document:
         self.category_id = category_id
         self.topic_id = topic_id
         self.file_name = file_name
-        self.tags = list()
+        self.tags = []
 
     @classmethod
-    def form_instance(cls, id: int, category: Category, topic: Topic, file_name: str):
+    def from_instances(cls, id: int, category: Category, topic: Topic, file_name: str):
         return cls(id, category.id, topic.id, file_name)
 
     def add_tag(self, tag_content: str):
@@ -20,13 +20,12 @@ class Document:
 
     def remove_tag(self, tag_content: str):
         if tag_content in self.tags:
-            self.tags.append(tag_content)
+            self.tags.remove(tag_content)
 
     def edit(self, file_name: str):
         self.file_name = file_name
 
     def __repr__(self):
-        """returns a string representation of a document in the format"""
         tags = ", ".join([tag for tag in self.tags])
         res = f"Document {self.id}: {self.file_name}; category {self.category_id}, topic {self.topic_id}, tags: {tags}"
         return res
