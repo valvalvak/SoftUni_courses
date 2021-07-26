3257ddfb2be6e7994069dfb315e0236076f771db
Valentin V. Vakrilov <vvakrilov@gmail.com>
2021-07-26 11:48:56 +0300

    homework update (w)

---
+++ 04. OOP/06.Polymorphism and Abstraction/01.Lab/05.Shapes/project/unit_test.py
@@ -0,0 +1,11 @@
+# # test circle
+# import unittest
+#
+# class ShapesTests(unittest.TestCase):
+#     def test(self):
+#         c = Circle(5)
+#         self.assertEqual(c.calculate_area(), 78.53981633974483)
+#         self.assertEqual(c.calculate_perimeter(), 31.41592653589793)
+#
+# if __name__ == "__main__":
+#     unittest.main()
