10ece3f4c8f56b4108669a993742fcdc7d135ec1
Valentin Vakrilov <vvakrilov@gmail.com>
2021-10-17 17:28:46 +0300

    Update Recursions.py

--- 03. Advanced/10.Tests/Recursions.py
+++ 03. Advanced/10.Tests/Recursions.py
@@ -1,27 +1,31 @@
-def max_numbers(numbers):
-    best_number = numbers[0]
+# def max_numbers(numbers):
+#     best_number = numbers[0]
+#
+#     for number in numbers:
+#         if best_number < number:
+#             best_number = number
+#
+#     return best_number
+#
+#
+# def max_numbers_rec(numbers):
+#     if len(numbers) == 1:
+#         return numbers[0]
+#
+#     x = numbers[-1]
+#     y = max_numbers_rec(numbers[:-1])
+#
+#     return x if y < x else y
 
-    for number in numbers:
-        if best_number < number:
-            best_number = number
 
-    return best_number
-
-
-def max_numbers_rec(numbers):
-    if len(numbers) == 1:
-        return numbers[0]
-
-    x = numbers[-1]
-    y = max_numbers_rec(numbers[:-1])
-
-    return x if y < x else y
-
-
-print(max_numbers([1, 2, 3, 4, 5]))
-
+# print(max_numbers([1, 2, 3, 4, 5]))
+a = [1, 2, 3, 4, 5]
 # print(max_numbers([5, 4, 3, 2, 1]))
-
-print(max_numbers_rec([1, 2, 3, 4, 5]))
+b = [1, 2, 3, 4, 5]
+# print(max_numbers_rec([1, 2, 3, 4, 5]))
 
 # print(max_numbers_rec([5, 4, 3, 2, 1]))
+
+print(a)
+print(a[-1])
+print(a[:-1])
