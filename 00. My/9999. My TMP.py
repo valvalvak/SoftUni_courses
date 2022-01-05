df15ba54c7ad0696f3436b73d149b083c3f3682e
Valentin V. Vakrilov <vvakrilov@gmail.com>
2022-01-05 08:54:29 +0200

    ok

---
+++ 00. My/9999. My TMP.py
@@ -0,0 +1,50 @@
+# class Elements:
+#     def __init__(self, name, value):
+#         self.name = name
+#         self.value = value
+#
+#
+# class Air(Elements):
+#     def __init__(self, name):
+#         super().__init__(name, 200)
+#
+#
+# class Earth(Elements):
+#     def __init__(self, name):
+#         super().__init__(name, 500)
+#
+#
+# class Fire(Elements):
+#     def __init__(self, name):
+#         super().__init__(name, 50)
+#
+#
+# class Water(Elements):
+#     def __init__(self, name):
+#         super().__init__(name, 400)
+#
+#
+# class ClassRepository:
+#     def __init__(self):
+#         self.elements = []
+#
+#
+# class Operator:
+#     def __init__(self):
+#         self.class_repository = ClassRepository()
+#
+#
+# air_1 = Air("Air_1")
+# air_2 = Air("Air_2")
+# earth_1 = Air("Earth_1")
+# earth_2 = Air("Earth_2")
+# fire_1 = Air("Fire_1")
+# fire_2 = Air("Fire_2")
+# water_1 = Air("Water_1")
+# water_2 = Air("Water_2")
+
+a = "Abra Cadabra"
+print(a)
+a = a.strip()
+print(a)
+
