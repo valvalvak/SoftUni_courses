a5e72d45427fde1bc7dc741e683b3dfc7d83f7ff
Tutzi <vvakrilov@gmail.com>
2022-01-09 12:31:35 +0200

    update

--- Retake_20_Dec_2021/project/supply/food.py
+++ Retake_20_Dec_2021/project/supply/food.py
@@ -1,9 +1,7 @@
-from abc import ABC
-
 from project.supply.supply import Supply
 
 
-class Food(Supply, ABC):
+class Food(Supply):
     DEFAULT_ENERGY: int = 25
 
     def __init__(self, name: str, energy=DEFAULT_ENERGY):
